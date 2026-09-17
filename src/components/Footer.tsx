4793b6fed91b650cb97abddc121307f8c8477ad5
mdmanirmia <51641027+mdmanirmia@users.noreply.github.com>
2026-09-17 12:11:33 -0230

    Link "After You Pay" page from Footer Resources

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -161,6 +161,7 @@ export default function Footer() {
               <li><Link href="/how-to-sell" className="hover:text-white">How to sell</Link></li>
               <li><Link href="/transfer-room" className="hover:text-white">The Transfer Room</Link></li>
               <li><Link href="/listing-review" className="hover:text-white">How Listings Are Reviewed</Link></li>
+              <li><Link href="/after-you-pay" className="hover:text-white">After You Pay</Link></li>
               <li><Link href="/buyer-faq" className="hover:text-white">Buyer FAQ</Link></li>
               <li><Link href="/seller-faq" className="hover:text-white">Seller FAQ</Link></li>
               <li><Link href="/payments" className="hover:text-white">Payment &amp; Withdrawal</Link></li>
